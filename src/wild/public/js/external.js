d6c1ee2afebd7cdf4d138d9f368da33d84ec6613
HaleyDchuk <hed248@nyu.edu>
2016-12-05 16:49:04 -0500

    Update external.js

--- src/wild/public/js/external.js
+++ src/wild/public/js/external.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			var mess = document.createTextNode("TESTING"); 
 			test.appendChild(mess); 
 			document.body.appendChild(test); 
-
+	//kayak validate 
 	var kayakButton = document.getElementById("kayakSubmit"); 
 	kayakButton.addEventListener('click', kayakValidate); 
 
@@ -28,6 +28,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			
 			error.innerHTML = "Please fill out all of the fields before submitting"; 
 			var element = document.getElementById('error-message').style.display = "inline"; 
+
+			
+
+		}
+	}
+	
+	
+	//backpack validate 
+	var backpackButton = document.getElementById("backpackSubmit"); 
+	backpackButton.addEventListener('click', backpackValidate); 
+
+	function backpackValidate(event){
+		
+		console.log("event"); 
+		console.log(event); 
+		 
+		var bName = document.getElementById('backpackName').value; 
+		var bQuantity = document.getElementById('backpackQuantity').value; 
+		var bPrice = document.getElementById('backpackPrice').value; 
+		var bDescription = document.getElementById('backpackDescription').value; 
+		var bUrl = document.getElementById('backpackUrl').value; 
+
+		if(bName == "" || bQuantity == "" || bPrice == "" || bDescription == "" || bUrl == "" || bName == null){
+			event.preventDefault(); 
+			console.log("we made it in the if for backpacking page!"); 
+			var error = document.getElementById('error-message-backpack'); 
+			//var message = document.createTextNode("Please fill out all of the fields before submitting"); 
+			//error.appendChild(message); 
+			
+			error.innerHTML = "Please fill out all of the fields before submitting"; 
+			var element = document.getElementById('error-message-backpack').style.display = "inline"; 
 
 			
 
