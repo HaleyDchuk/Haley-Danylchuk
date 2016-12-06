64cdf4cf38210aff106368edc68417d35c688ddb
HaleyDchuk <hed248@nyu.edu>
2016-12-06 15:57:25 -0500

    Update externalDestinationWeather.js

--- src/wild/public/js/externalDestinationWeather.js
+++ src/wild/public/js/externalDestinationWeather.js
@@ -18,7 +18,7 @@ function loadContent() {
   };
 
   // Prepare the request
-  xhr.open("GET", "api-request.php", true);
+  xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=Paihia,nz&appid=c5ddb19e5df75b259cd0e5aaafc1f2c3", true);
   xhr.send();
 }
 
