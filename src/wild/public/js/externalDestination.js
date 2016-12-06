22b5af09c5a7b81a2b3fd45b0a13b6a8d7313ae0
HaleyDchuk <hed248@nyu.edu>
2016-12-06 13:52:41 -0500

    Update externalDestination.js

--- src/wild/public/js/externalDestination.js
+++ src/wild/public/js/externalDestination.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+//document.addEventListener("DOMContentLoaded", function(event) {
 	var mapContainer = document.getElementById('map');
 
 function init() {
@@ -26,36 +26,36 @@ window.addEventListener('load', loadScript, false);
 
 
 	
-	//destinations validate 
-	var destinationButton = document.getElementById("destinationSubmit"); 
-	destinationButton.addEventListener('click', destinationValidate); 
+// 	//destinations validate 
+// 	var destinationButton = document.getElementById("destinationSubmit"); 
+// 	destinationButton.addEventListener('click', destinationValidate); 
 
-	function destinationValidate(event){
+// 	function destinationValidate(event){
 		
-		console.log("event"); 
-		console.log(event); 
+// 		console.log("event"); 
+// 		console.log(event); 
 		 
-		var dLocation = document.getElementById('destinationLocation').value; 
+// 		var dLocation = document.getElementById('destinationLocation').value; 
 
-		var dDescription = document.getElementById('destinationDescription').value; 
-		var dUrl = document.getElementById('destinationUrl').value; 
+// 		var dDescription = document.getElementById('destinationDescription').value; 
+// 		var dUrl = document.getElementById('destinationUrl').value; 
 
-		if(dLocation == "" || dDescription == "" || dUrl == ""){
-			event.preventDefault(); 
-			console.log("we made it in the if for destination page!"); 
-			var destinationError = document.getElementById('error-message-destination'); 
-			//var message = document.createTextNode("Please fill out all of the fields before submitting"); 
-			//error.appendChild(message); 
+// 		if(dLocation == "" || dDescription == "" || dUrl == ""){
+// 			event.preventDefault(); 
+// 			console.log("we made it in the if for destination page!"); 
+// 			var destinationError = document.getElementById('error-message-destination'); 
+// 			//var message = document.createTextNode("Please fill out all of the fields before submitting"); 
+// 			//error.appendChild(message); 
 			
-		destinationError.innerHTML = "Please fill out all of the fields before submitting"; 
-			var element = document.getElementById('error-message-destination').style.display = "inline"; 
+// 		destinationError.innerHTML = "Please fill out all of the fields before submitting"; 
+// 			var element = document.getElementById('error-message-destination').style.display = "inline"; 
 
 			
 
-		}
-	}
+// 		}
+// 	}
 
 
-    console.log("DOM fully loaded and parsed");
-  });
+//     console.log("DOM fully loaded and parsed");
+//   });
 
