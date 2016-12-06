d3a480897eb8c312a33da6856b51d95be6a0cb4a
HaleyDchuk <hed248@nyu.edu>
2016-12-06 15:37:50 -0500

    Delete api-request-source.php

--- src/wild/public/api-request-source.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $request = 'http://api.openweathermap.org/data/2.5/weather?id=5128581&units=imperial&APPID=c5ddb19e5df75b259cd0e5aaafc1f2c3';
-  $response = file_get_contents($request);
-  echo $response;
-?>
