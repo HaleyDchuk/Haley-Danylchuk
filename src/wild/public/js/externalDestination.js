95e0bbe7ebd58ef5a8daa18c762bf8b44cb5bb49
HaleyDchuk <hed248@nyu.edu>
2016-12-06 15:22:55 -0500

    Update externalDestination.js

--- src/wild/public/js/externalDestination.js
+++ src/wild/public/js/externalDestination.js
@@ -1,5 +1,12 @@
 //document.addEventListener("DOMContentLoaded", function(event) {
-	var mapContainer = document.getElementById('map');
+	
+var url = "http://api.openweathermap.org/data/2.5/weather?q=Paihia,nz&appid=c5ddb19e5df75b259cd0e5aaafc1f2c3"; 
+console.log("weather json"); 
+console.log(url); 
+
+
+
+var mapContainer = document.getElementById('map');
 
 function init() {
   var mapOptions = {
