6f25430d8520e260f19b3eebe5f13bb0c7e38c8d
HaleyDchuk <hed248@nyu.edu>
2016-12-06 20:08:01 -0500

    Update general.js

--- src/wild/public/js/general.js
+++ src/wild/public/js/general.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+//document.addEventListener("DOMContentLoaded", function(event) {
 	
 	var classes = [{title: "Applied Internet Technology", section: "001", id: "CSCI-UA.0480", day: "Tues/Thurs", time: "9:30 am", professor: "Joe Versoza"}, {title: "Operating Systems", section: "001", id: "CSCI-UA.202", day: "Tues/Thurs", time: "2:00 pm", professor: "Alan Gottlieb"}, {title: "Basic Algorithms", section: "002", id: "CSCI-UA.200", day: "Tues/Thurs", time: "3:30 pm", professor: "Alan Siegel"}];  
 	var thisClass = "Applied Internet Technology"; 
@@ -16,5 +16,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	
 
 
-    console.log("DOM fully loaded and parsed");
-  });
+    //console.log("DOM fully loaded and parsed");
+  //});
