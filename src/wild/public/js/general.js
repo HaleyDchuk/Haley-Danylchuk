9b826b723c700261fdd6079338f2ac2a7695dc50
HaleyDchuk <hed248@nyu.edu>
2016-12-08 15:52:53 -0500

    Update general.js

--- src/wild/public/js/general.js
+++ src/wild/public/js/general.js
@@ -15,12 +15,12 @@
 // console.log(filtered); 
 
 	console.log("WHAT"); 
-console.log(classes.title); 
+//console.log(classes.title); 
 	function importantClass(class) {
 		return class.title === "Applied Internet Technology"; 	
 	}
 
-	var filtered = classes.filter(importantClass); 
+	var filtered = [{title: "Applied Internet Technology", section: "001", id: "CSCI-UA.0480", day: "Tues/Thurs", time: "9:30 am", professor: "Joe Versoza"},  {title: "Operating Systems", section: "001", id: "CSCI-UA.202", day: "Tues/Thurs", time: "2:00 pm", professor: "Alan Gottlieb"}, {title: "Basic Algorithms", section: "002", id: "CSCI-UA.200", day: "Tues/Thurs", time: "3:30 pm", professor: "Alan Siegel"}].filter(importantClass); 
 	console.log("FILTERED"); 
 	console.log(filtered); 
 
